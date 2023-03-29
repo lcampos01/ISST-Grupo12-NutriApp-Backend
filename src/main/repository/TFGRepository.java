239524ec49b3d87d9060f6bb926f7a59e528272e
APerJim <lonspj@gmail.com>
2023-03-29 17:55:54 +0200

    Primera conexion con DB

--- src/main/repository/TFGRepository.java
+++
@@ -1,9 +0,0 @@
-package es.upm.dit.isst.tfgapi.repository;
-
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-import es.upm.dit.isst.tfgapi.model.TFG;
-
-public interface TFGRepository extends CrudRepository<TFG, String> {
-    List<TFG> findByTutor(String tutor);
-}
