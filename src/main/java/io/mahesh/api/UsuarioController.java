a7e8d5a7cbfe77e91f011598739fe6b230566c95
DavidPBen <david.plaza.benito@alumnos.upm.es>
2023-04-04 19:10:06 +0200

    desarrollo final

--- src/main/java/io/mahesh/api/UsuarioController.java
+++ src/main/java/io/mahesh/api/UsuarioController.java
@@ -3,8 +3,6 @@ package io.mahesh.api;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import javax.swing.InputMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,8 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.net.URL;
 import java.net.HttpURLConnection;
-import java.io.Reader;
-import java.io.IOException;
 
 @RestController
 
@@ -36,14 +32,19 @@ public class UsuarioController {
     }
 
     @GetMapping("/search-product/scan/{codigo}")
-    public String getScanning(@PathVariable(value = "codigo") Double codigo) {
+    public String getScanning(@PathVariable(value = "codigo") String codigo) {
         try{
+            StringBuilder result = new StringBuilder();
             URL url = new URL("https://world.openfoodfacts.org/api/v2/product/"+codigo);
             HttpURLConnection request = (HttpURLConnection) url.openConnection();
-            Reader r = new InputStreamReader(request.getInputStream());
-            BufferedReader br = new BufferedReader(r);
-            String line = br.readLine();
-            return line;
+            request.setRequestMethod("GET");
+            BufferedReader rd = new BufferedReader(new InputStreamReader(request.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            rd.close();
+            return result.toString();
         }
         catch(Exception e){
             System.err.println("Error: " + e.getMessage());
@@ -51,15 +52,20 @@ public class UsuarioController {
         }
     }
 
-    @GetMapping("/search-product/search/{product_name}")
-    public String getSearch(@PathVariable(value = "product_name") String product_name) {
+    @GetMapping("/search-product/search/{product_category}")
+    public String getSearch(@PathVariable(value = "product_category") String product_category) {
         try{
-            URL url = new URL("https://world.openfoodfacts.org/categorie/"+product_name+".json");
+            StringBuilder result = new StringBuilder();
+            URL url = new URL("https://es.openfoodfacts.org/categoria/"+product_category+".json");
             HttpURLConnection request = (HttpURLConnection) url.openConnection();
-            Reader r = new InputStreamReader(request.getInputStream());
-            BufferedReader br = new BufferedReader(r);
-            String line = br.readLine();
-            return line;
+            request.setRequestMethod("GET");
+            BufferedReader rd = new BufferedReader(new InputStreamReader(request.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            rd.close();
+            return result.toString();
         }
         catch(Exception e){
             System.err.println("Error: " + e.getMessage());
