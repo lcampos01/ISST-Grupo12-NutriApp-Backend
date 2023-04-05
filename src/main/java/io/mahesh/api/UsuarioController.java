abeeed0fee0fea6add5c566723c818d5d8431311
DavidPBen <david.plaza.benito@alumnos.upm.es>
2023-04-05 19:35:37 +0200

    modificar datos del usuario

--- src/main/java/io/mahesh/api/UsuarioController.java
+++ src/main/java/io/mahesh/api/UsuarioController.java
@@ -2,16 +2,32 @@ package io.mahesh.api;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import java.net.URL;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Optional;
 import java.net.HttpURLConnection;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @RestController
 
@@ -21,18 +37,56 @@ public class UsuarioController {
     private UsuarioRepository usuarioRepository;
 
     @PostMapping("/register")
-    public Usuario Register(@RequestBody Usuario usuario) {
+    public Usuario Register(@RequestBody Usuario usuario) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, UnsupportedEncodingException {
+        //final byte[] bytes = usuario.contraseña.getBytes("UTF-8");
+        final String frase = "1234567890123456";
+        final MessageDigest digest = MessageDigest.getInstance("SHA");
+        digest.update(frase.getBytes("UTF-8"));
+        final SecretKeySpec key = new SecretKeySpec(digest.digest(), 0, 16, "AES");
+        final Cipher aes = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        aes.init(Cipher.ENCRYPT_MODE, key);
+	    //final byte[] cifrado = aes.doFinal(bytes);
         return usuarioRepository.save(usuario);
     }
 
     @PostMapping("/login")
-    public Usuario Login(@RequestBody Usuario usuario) {
-        Usuario Usuario_antiguo = usuarioRepository.findByEmailAndContraseña(usuario.email, usuario.contraseña);
-        return Usuario_antiguo;
+    public ResponseEntity Login(@RequestBody Usuario usuario) {
+        Usuario usuario_antiguo = usuarioRepository.findByEmailAndContraseña(usuario.email, usuario.contraseña);
+        if(usuario_antiguo != null){
+            Usuario usuario_nuevo = new Usuario();
+            usuario_nuevo.id = usuario_antiguo.id;
+            usuario_nuevo.nombre = usuario_antiguo.nombre;
+            usuario_nuevo.email = usuario_antiguo.email;
+            usuario_nuevo.altura = usuario_antiguo.altura;
+            usuario_nuevo.peso = usuario_antiguo.peso;
+            usuario_nuevo.sexo = usuario_antiguo.sexo;
+            usuario_nuevo.fecha_nacimiento = usuario_antiguo.fecha_nacimiento;
+            usuario_nuevo.actividad_diaria = usuario_antiguo.actividad_diaria;
+            usuario_nuevo.isAdmin = usuario_antiguo.isAdmin;
+            return ResponseEntity.ok().body(usuario_nuevo);
+        } else{
+            return new ResponseEntity<Usuario>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/modify-user/{id}")
+    public ResponseEntity updateUser(@PathVariable Integer id, @RequestBody Usuario Usuario) {
+        return usuarioRepository.findById(id).map(usuario -> {
+            usuario.setNombre(Usuario.getNombre());
+            usuario.setEmail(Usuario.getEmail());
+            usuario.setAltura(Usuario.getAltura());
+            usuario.setPeso(Usuario.getPeso());
+            usuario.setSexo(Usuario.getSexo());
+            usuario.setFecha_nacimiento(Usuario.getFecha_nacimiento());
+            usuario.setActividad_diaria(Usuario.getActividad_diaria());
+            usuario.setIsAdmin(Usuario.getIsAdmin());
+            usuarioRepository.save(usuario);
+            return ResponseEntity.ok().body(usuario);
+        }).orElse(new ResponseEntity<Usuario>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/search-product/scan/{codigo}")
-    public String getScanning(@PathVariable(value = "codigo") String codigo) {
+    public ResponseEntity getScanning(@PathVariable(value = "codigo") String codigo) {
         try{
             StringBuilder result = new StringBuilder();
             URL url = new URL("https://world.openfoodfacts.org/api/v2/product/"+codigo);
@@ -44,16 +98,15 @@ public class UsuarioController {
                 result.append(line);
             }
             rd.close();
-            return result.toString();
+            return ResponseEntity.ok().body(result.toString()); 
         }
         catch(Exception e){
-            System.err.println("Error: " + e.getMessage());
-            return null;
+            return new ResponseEntity<Usuario>(HttpStatus.NOT_FOUND);
         }
     }
 
     @GetMapping("/search-product/search/{product_category}")
-    public String getSearch(@PathVariable(value = "product_category") String product_category) {
+    public ResponseEntity getSearch(@PathVariable(value = "product_category") String product_category) {
         try{
             StringBuilder result = new StringBuilder();
             URL url = new URL("https://es.openfoodfacts.org/categoria/"+product_category+".json");
@@ -65,11 +118,10 @@ public class UsuarioController {
                 result.append(line);
             }
             rd.close();
-            return result.toString();
+            return ResponseEntity.ok().body(result.toString()); 
         }
         catch(Exception e){
-            System.err.println("Error: " + e.getMessage());
-            return null;
+            return new ResponseEntity<Usuario>(HttpStatus.NOT_FOUND);
         }
     }
 }
